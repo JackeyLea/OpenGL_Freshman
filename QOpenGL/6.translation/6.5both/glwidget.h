52f1b8c74c6f54ad153d7240af14dda5b387955d
Jackey Lea <1768478912@qq.com>
2022-07-07 14:20:51 +0800

    添加平移代码

---
+++ QOpenGL/6.translation/6.5both/glwidget.h
@@ -0,0 +1,41 @@
+#ifndef GLWIDGET_H
+#define GLWIDGET_H
+
+#include <QWidget>
+#include <QOpenGLWidget>
+#include <QOpenGLFunctions>
+#include <QOpenGLShaderProgram>
+#include <QOpenGLBuffer>
+#include <QOpenGLVertexArrayObject>
+#include <QKeyEvent>
+#include <QApplication>
+#include <QSurfaceFormat>
+#include <QDebug>
+
+class GLWidget : public QOpenGLWidget,public QOpenGLFunctions
+{
+    Q_OBJECT
+
+public:
+    GLWidget(QWidget *parent = 0);
+    ~GLWidget();
+
+protected:
+    void initializeGL();
+    void resizeGL(int w, int h);
+    void paintGL();
+
+    void keyPressEvent(QKeyEvent *event);
+
+private:
+    bool mFullScreen;
+
+    QOpenGLShaderProgram *program;
+
+    GLuint m_vboIds[4];
+
+    QMatrix4x4 m_projection;
+    QMatrix4x4 m_modelView;
+};
+
+#endif // GLWIDGET_H
--- QOpenGL/6.translation/6.6translate/mainwindow.h
+++ QOpenGL/6.translation/6.6translate/mainwindow.h
@@ -2,21 +2,28 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QResizeEvent>
+#include <QKeyEvent>
 
-QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
-QT_END_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+protected:
+    void keyPressEvent(QKeyEvent *event);
+
 private slots:
+    void on_checkTriangle_clicked(bool checked);
+
+    void on_checkQuad_clicked(bool checked);
+
     void on_btnUp_clicked();
 
     void on_btnDown_clicked();
@@ -29,7 +36,12 @@ private slots:
 
     void on_btnClose_clicked();
 
+    void on_btnReset_clicked();
+
 private:
     Ui::MainWindow *ui;
+
+    bool mFullScreen;
 };
+
 #endif // MAINWINDOW_H
