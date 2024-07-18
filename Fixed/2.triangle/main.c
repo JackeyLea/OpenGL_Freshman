b66f6b654d80be56508742ab92c63900c94e5f2f
Jackey Lea <1768478912@qq.com>
2024-07-18 19:56:42 +0800

    add new code

---
+++ Fixed/2.triangle/main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <GLFW/glfw3.h>
+
+static void glfw_error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Glfw Error %d: %s\n", error, description);
+}
+
+int main(int, char**)
+{
+    // Setup window
+    glfwSetErrorCallback(glfw_error_callback);
+    if (!glfwInit())
+        return 1;
+
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "GLFW Window", NULL, NULL);
+    if (window == NULL)
+        return 1;
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // Enable vsync
+
+    // Main loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // Poll and handle events (inputs, window resize, etc.)
+        glfwPollEvents();
+
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(0.0f, 1.0f, 1.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        //display func
+        glBegin(GL_TRIANGLES);
+        	glVertex3f(-1.0,-1.0f,0);
+		glVertex3f(0,1,0);
+		glVertex3f(1.0,-1.0f,0);
+        glEnd();
+
+        glfwMakeContextCurrent(window);
+        glfwSwapBuffers(window);
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
+}
